415d9cf844afcbf7ef63615bb0f6b076f5daa60c
platers <platers81@gmail.com>
2022-05-25 12:36:27 -0400

    update tests

--- tests/test_tinyplaylists.py
+++ tests/test_tinyplaylists.py
@@ -19,7 +19,7 @@ def test_empty_library(empty_library):
 
 
 @pytest.fixture
-def library_with_playlists(empty_library):
+def dir_with_playlists(empty_library):
     os.mkdir("test_lib/playlist1")
     os.mkdir("test_lib/playlist2")
 
@@ -27,12 +27,18 @@ def library_with_playlists(empty_library):
     shutil.copy("tests/audiofiles/2-seconds.mp3", "test_lib/playlist1/b.mp3")
 
     yield empty_library
+
     shutil.rmtree("test_lib/playlist1")
     shutil.rmtree("test_lib/playlist2")
 
 
+@pytest.fixture
+def library_with_playlists(dir_with_playlists):
+    return TinyPlaylists(dir_with_playlists)
+
+
 def test_library_with_playlists(library_with_playlists):
-    tpl = TinyPlaylists(library_with_playlists)
+    tpl = library_with_playlists
     assert len(tpl.playlists) == 2
     assert "playlist1" in tpl.playlists
     assert "playlist2" in tpl.playlists
@@ -42,8 +48,6 @@ def test_library_with_playlists(library_with_playlists):
 
 
 def test_uuid_added_to_file_name(library_with_playlists):
-    tpl = TinyPlaylists(library_with_playlists)
-    pl1 = tpl.get_playlist("playlist1")
     valid_file_name = re.compile(r"^.*-([0-9a-f]{32}).*$")
     for file in os.listdir("test_lib/playlist1"):
         assert valid_file_name.match(file)
